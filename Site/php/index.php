c00d2bd9d24880526b9306b8e00b6e18d7643c11
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-09-15 12:40:35 +0200

    work work work

---
+++ Site/php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>VrijheidVoorIeder.</title>
+    <link rel="stylesheet" href="../css/style-index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="grid">
+            <div class="header-left">
+                <a href="../php/wie-zijn-wij.php">Wie zijn wij?</a>
+                <a href="../php/standpunten.php">Standpunten.</a>
+            </div>
+            <div class="logo">
+                <a href="../php/index.php"><img src="../../logo/logo_small_icon_only.png"></a>
+            </div>
+            <div class="header-right">
+                <a href="../php/agenda.php">Agenda.</a>
+                <a href="../php/contact.php">Contact.</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
