5b82db4c57beb5cc74ef740f1be12c097bc0266e
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-09-20 14:07:40 +0200

    update

--- Site/php/agenda.php
+++ Site/php/agenda.php
@@ -8,17 +8,18 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
+<header>
         <div class="grid">
             <div class="header-left">
                 <a href="../php/wie-zijn-wij.php">Wie zijn wij?</a>
                 <a href="../php/standpunten.php">Standpunten.</a>
+                <a href="../php/agenda.php">Agenda.</a>
             </div>
             <div class="logo">
                 <a href="../php/index.php"><img src="../../logo/logo_small_icon_only.png"></a>
             </div>
             <div class="header-right">
-                <a href="../php/agenda.php">Agenda.</a>
+                <a href="../php/agenda.php">Log In/Register.</a>
                 <a href="../php/contact.php">Contact.</a>
             </div>
         </div>
--- Site/php/index.php
+++ Site/php/index.php
@@ -6,6 +6,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -13,24 +16,22 @@
             <div class="header-left">
                 <a href="../php/wie-zijn-wij.php">Wie zijn wij?</a>
                 <a href="../php/standpunten.php">Standpunten.</a>
+                <a href="../php/agenda.php">Agenda.</a>
             </div>
             <div class="logo">
                 <a href="../php/index.php"><img src="../../logo/logo_small_icon_only.png"></a>
             </div>
             <div class="header-right">
-                <a href="../php/agenda.php">Agenda.</a>
+                <a href="../php/agenda.php">Log In/Register.</a>
                 <a href="../php/contact.php">Contact.</a>
             </div>
         </div>
     </header>
-    <br>
     <main>
-        <div class="titel">
-            <h1>Vrijheid Voor Ieder</h1>
+        <div class="banner">
+            <div class="inner-banner">
+                <a href="#"><img src="../../logo/logo_small copy.png"></a>
+            </div>
         </div>
-        <div class="foto1">
-            <img src="../../logo/Dodenherdenking.jpg">
-        </div>
-    </main>
 </body>
 </html>
