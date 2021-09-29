8a8483b57d66c92d9121ba584b4f769deae30aee
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-09-29 12:14:44 +0200

    update

--- Site/php/index.php
+++ Site/php/index.php
@@ -9,6 +9,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hind&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -29,7 +32,73 @@
     </header>
     <main>
         <div class="banner">
-            <img src="../../banner/banner.png">
+            <div class="img1"><img src="../../logo/Vlag_Nederland_960_Foto_Mabel_Amber_Pixabay.jpg"></div>
+            <div class="txt1">
+                <div class="innertxt1">
+                    <h1>Vrijheid Voor Ieder</h1>
+                    <p>VrijheidVoorIeder is een partij die staat voor de vrijheid van iederen! Man, vrouw en alles er tussen in!</p>
+                    <a href="../php/loginregister.php"><button class="button1">Wordt lid!</button></a>
+                </div>
+            </div>
+        </div>
+        <div class="standpunten">
+            <h1 class="titelstandpunten">Onze standpunten.</h1>
+            <div class="grid2">
+                <div class="wz">
+                    <h2>Werk & Zekerheid.</h2>
+                    <a><img src="../../logo/briefcase.svg"></a>
+                </div>
+
+                <div class="zg">
+                    <h2>Zorg & Gezondheid.</h2>
+                    <a><img src="../../logo/fi-rr-heart.svg"></a>
+                </div>
+
+                <div class="mi">
+                    <h2>Migratie & Integratie.</h2>
+                    <a><img src="../../logo/fi-rr-home-location-alt.svg"></a>
+                </div>
+
+                <div class="vr">
+                    <h2>Veiligheid & Recht.</h2>
+                    <a><img src="../../logo/fi-rr-shield-check.svg"></a>
+                </div>
+            </div>
+        </div>
+        <div class="onzemensen">
+            <h1>Onze mensen.</h1>
+            <h2>De partijleider.</h2>
+            <div class="partlead">
+                <a><img src="../../logo/istockphoto-517998264-612x612.jpg"></a>
+            </div>
+            <div class="partleaddesc">
+                <h3>Frans Manfred</h3>
+                <p>Frans is een sympatieke vent geboren in Nederland. Om meer over Frans te weten te komen klik op zijn foto!</p>
+            </div>
+            <div class="rest">
+                <div class="een">
+                    <a><img src="../../logo/istockphoto-517998264-612x612.jpg"></a>
+                    <h3>Yefrem Georgry</h3>
+                    <p>Yefrem is een geharde vent uit Rusland. Om meer over Yefrem te weten te komen klik op zijn foto!</p>
+                </div>
+                <div class="twee">
+                    <a><img src="../../logo/istockphoto-517998264-612x612.jpg"></a>
+                    <h3>A'isha Mouna</h3>
+                    <p>A'isha is een sympatieke dame geboren in Tunesië. Om meer over A'isha te weten te komen klik op haar foto!</p>
+                </div>
+                <div class="drie">
+                    <a><img src="../../logo/istockphoto-517998264-612x612.jpg"></a>
+                    <h3>Seo-Yeon Min-Su</h3>
+                    <p>Seo-Yeon is een jonge dame geboren in Noord-Korea. Om meer over Frans te weten te komen klik op haar foto!</p>
+                </div>
+            </div>
+            <div class="buttontxt">
+                <h4>Om onze andere partij leden te bekijken en een goed overzicht van al onze leden te krijgen klik op de knop hier onder!</h4>
+                <a href="../php/wie-zijn-wij.php"><button class="button2">Klik hier!</button></a>
+            </div>
+        </div>
+        <div class="contact">
+
         </div>
     </main>
 </body>
