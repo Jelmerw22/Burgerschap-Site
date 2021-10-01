356c7a7e0038098887f407631424e54f82839e3b
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-10-01 11:13:45 +0200

    addon

---
+++ partijleden/a'isha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A'isha Mouna</title>
+</head>
+<body>
+<header>
+        <div class="grid">
+            <div class="header-left">
+                <a href="../php/wie-zijn-wij.php">Wie zijn wij?</a>
+                <a href="../php/standpunten.php">Standpunten.</a>
+                <a href="../php/agenda.php">Agenda.</a>
+            </div>
+            <div class="logo">
+                <a href="../php/index.php"><img src="../../logo/logo_small_icon_only.png"></a>
+            </div>
+            <div class="header-right">
+                <a href="../php/loginregister.php">Log In/Register.</a>
+                <a href="../php/contact.php">Contact.</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
