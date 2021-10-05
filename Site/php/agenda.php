416a695f5a014a5867c8ac23f10975fc0f2feb7b
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-10-05 13:56:43 +0200

    update

--- Site/php/agenda.php
+++ Site/php/agenda.php
@@ -6,6 +6,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="../css/calendar.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <header>
@@ -25,7 +26,7 @@
         </div>
     </header>
     <main>
-
+        
     </main>
     <footer>
         <div class="contact">
