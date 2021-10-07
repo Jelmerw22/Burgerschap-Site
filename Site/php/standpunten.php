a9d7380e5115135e1cdce12140033f45dec6e3ae
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-10-07 12:29:49 +0200

    standpunten

--- Site/php/standpunten.php
+++ Site/php/standpunten.php
@@ -33,18 +33,79 @@
         </Div>
         <Div class="standpunten">
             <li class="gridlist">
-                <ul>Werk & Zekerheid.</ul>
-                <ul>Veiligheid & Recht.</ul>
-                <ul>Zorg & Gezondheid.</ul>
-                <ul>Migratie & Integratie.</ul>
-                <ul>Economie & Ondernemen.</ul>
-                <ul>Wonen & Vervoer.</ul>
-                <ul>Klimaat & Duurzaamheid</ul>
-                <ul>Onderwijs & Cultuur.</ul>
-                <ul>Overheid & Democratie.</ul>
-                <ul>Defensie & Buitenland.</ul>
-                <ul>Landbouw & Natuur.</ul>
-                <ul>Vrijheid & Waarden</ul>
+                <ul>
+                    Werk & Zekerheid.
+                    <ol>Ondernemers vrijheid geven in kiesen wat ze willen doen.</ol>
+                    <ol>Beginnende ondernemers ondersteunen.</ol>
+                    <img src="../../logo/briefcase.svg" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Veiligheid & Recht.
+                    <ol>Taakstraffen verminderen en meer gevangenis straffen.</ol>
+                    <ol>Meer eigen rechten.</ol>
+                    <img src="../../logo/fi-rr-shield-check.svg" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Zorg & Gezondheid.
+                    <ol>Ieder dezelfde zorg.</ol>
+                    <ol>Zorg voor iederen toegankelijk maken.</ol>
+                    <img src="../../logo/fi-rr-heart.svg" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Migratie & Integratie.
+                    <ol>Mensen met Nederlandse nationaliteit altijd verzekert van onderdak.</ol>
+                    <ol>Nederlandse nationaliteit makkelijker te verkrijgen.</ol>
+                    <img src="../../logo/fi-rr-home-location-alt.svg" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Economie & Ondernemen.
+                    <ol>Overheid uitgaves verlagen.</ol>
+                    <ol>Belasting verlagen.</ol>
+                    <img src="../../logo/euro.png" alt="" width="62.5" height="auto">
+
+                </ul>
+                <ul>
+                    Wonen & Vervoer.
+                    <ol>Veel wonen bij bowen maar natuur gebieden in tact houden.</ol>
+                    <ol>Meer tunnels om files en weg drukte te bestrijden.</ol>
+                    <img src="../../logo/home.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Klimaat & Duurzaamheid.
+                    <ol>Over gaan op kern-energie.</ol>
+                    <ol>Minder uitstoor veroorzaken.</ol>
+                    <img src="../../logo/temperature.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Onderwijs & Cultuur.
+                    <ol>Goede scholen voor iedereen beschikbaar.</ol>
+                    <ol>Altijd een perspectie voor toekomst houden.</ol>
+                    <img src="../../logo/history.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Overheid & Democratie.
+                    <ol>Een overheid die zich alleen laat zien als het echt nodig is.</ol>
+                    <ol>Tussentijdse verkiezingen voor gemeentes.</ol>
+                    <img src="../../logo/city-hall.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Defensie & Buitenland.
+                    <ol>Sterke band opbouwen met EU.</ol>
+                    <ol>Nederlandse militaire beschikbaar stellen voor de belange van EU en Nederland.</ol>
+                    <img src="../../logo/helmet.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Landbouw & Natuur.
+                    <ol>Subsidies mogelijk voor boeren in moeilijke tijden.</ol>
+                    <ol>Zoveel mogelijk grote natuur gebieden in tact houden.</ol>
+                    <img src="../../logo/planting.png" alt="" width="62.5" height="auto">
+                </ul>
+                <ul>
+                    Vrijheid & Waarden.
+                    <ol>Vrijheid voor ieder.</ol>
+                    <ol>Hoge straffe op elke vorm van discriminatie.</ol>
+                    <img src="../../logo/dove.png" alt="" width="62.5" height="auto">
+                </ul>
             </li>
         </Div>
     </main>
